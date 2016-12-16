6dc72ed1991a8ead11797eee6860eace18315848
Joanna Roczniak <roczniak.joanna@gmail.com>
2016-12-17 00:12:19 +0100

    Add kaazing library to retrieve Server Sent Event from server and ButterKnife. Implement AsyncTask to retrieve Server Sent Event from server using kaazing. Show elements from stream on the UI. Update gradle.

--- app/src/main/java/pl/edu/agh/smiling/driftdetector/MainActivity.java
+++ app/src/main/java/pl/edu/agh/smiling/driftdetector/MainActivity.java
@@ -1,6 +1,6 @@
 package pl.edu.agh.smiling.driftdetector;
 
-import android.support.design.widget.Snackbar;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,154 +8,111 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import org.kaazing.net.sse.SseEventReader;
+import org.kaazing.net.sse.SseEventSource;
+import org.kaazing.net.sse.SseEventSourceFactory;
+import org.kaazing.net.sse.SseEventType;
 
-import okhttp3.ResponseBody;
-import pl.edu.agh.smiling.driftdetector.rest.ServiceGenerator;
-import pl.edu.agh.smiling.driftdetector.rest.SmilingClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    @BindView(R.id.text_from_server)
     TextView textView;
+
+    @BindView(R.id.button_stop)
+    Button buttonStop;
+    @BindView(R.id.button_start)
+    Button buttonStart;
+
+    private final String host = "http://192.168.43.203:5000/subscribe";
+    private SseEventSource sseEventSource;
+
+    private AsyncTask asyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
         textView = (TextView) findViewById(R.id.text_from_server);
 
-        Button buttonStart = (Button) findViewById(R.id.button_start);
-        Button buttonInput = (Button) findViewById(R.id.button_input);
-        Button buttonRead = (Button) findViewById(R.id.button_read);
-
-        buttonRead.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                read("gender_phone.csv");
-            }
-        });
+        //TODO: Enable streaming from various sources
 
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                DriftDetector driftDetector =
-
-                for(int i = 0; i < 10; i++){
-
-                    startStreaming(String.valueOf(i));
-                }
+                //TODO: Check if there is internet connection
+                asyncTask = new HttpRequest().execute(host);
             }
         });
 
-        buttonInput.setOnClickListener(new View.OnClickListener() {
+        buttonStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                input();
+                asyncTask.cancel(true);
             }
         });
     }
 
-    private void read(String filename) {
-        SmilingClient client = ServiceGenerator.getClient();
-//
-        Call<String> call = client.read(filename);
-        call.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                if (response.isSuccessful()) {
-                    textView.setText(response.body());
-                } else {
-                    textView.setText("NO TEXT FROM SERVER");
-                }
-            }
+    public class HttpRequest extends AsyncTask<String, String, Void> {
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                final SseEventSourceFactory sseEventSourceFactory = SseEventSourceFactory.createEventSourceFactory();
+                sseEventSource = sseEventSourceFactory.createEventSource(new URI(params[0]));
+                sseEventSource.connect();
 
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                Log.e(TAG, "onFailure: ", t);
-                if (t.toString().contains("ConnectException")) {
-                    Snackbar.make(findViewById(R.id.activity_main), "FAILURE", Snackbar.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
+                final SseEventReader sseEventReader = sseEventSource.getEventReader();
 
-    private void startStreaming(String line) {
-        SmilingClient client = ServiceGenerator.getClient();
-//
-        Call<String> call = client.startStreaming(line);
-        call.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                if (response.isSuccessful()) {
-                    textView.setText(response.body());
-                } else {
-                    textView.setText("NO TEXT FROM SERVER");
+                SseEventType type = sseEventReader.next();
+                while (type != SseEventType.EOS) {
+                    Log.d(TAG, "onClick: new event");
+                    if (type != null && type.equals(SseEventType.DATA)) {
+                        CharSequence data = sseEventReader.getData();
+                        //TODO: Send new item to DriftDetector method and show when concept drift occurs
+                        publishProgress(data.toString());
+                        Log.d(TAG, "onClick: " + data.toString());
+                    } else {
+                        Log.d(TAG, "onClick: type null or not data: " + type);
+                        publishProgress("empty");
+                    }
+                    type = sseEventReader.next();
+                    Thread.sleep(300);
                 }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            return null;
+        }
 
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                Log.e(TAG, "onFailure: ", t);
-                if (t.toString().contains("ConnectException")) {
-                    Snackbar.make(findViewById(R.id.activity_main), "FAILURE", Snackbar.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
+        @Override
+        protected void onProgressUpdate(String... data) {
+            textView.setText(data[0]);
+            super.onProgressUpdate(data);
+        }
 
-    private void input() {
-        SmilingClient client = ServiceGenerator.getClient();
-//
-        Call<String> call = client.input("result.csv");
-        call.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                if (response.isSuccessful()) {
-                    textView.setText(response.body());
-                } else {
-                    textView.setText("NO TEXT FROM SERVER");
-                }
+        @Override
+        protected void onCancelled() {
+            try {
+                sseEventSource.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                Log.e(TAG, "onFailure: ", t);
-                if (t.toString().contains("ConnectException")) {
-                    Snackbar.make(findViewById(R.id.activity_main), "FAILURE", Snackbar.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
-
-    private void startStream() {
-        SmilingClient client = ServiceGenerator.getClient();
-//
-        Call<String> call = client.startStream();
-        call.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                if (response.isSuccessful()) {
-                    textView.setText(response.body());
-                } else {
-                    textView.setText("NO TEXT FROM SERVER");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                Log.e(TAG, "onFailure: ", t);
-                if (t.toString().contains("ConnectException")) {
-                    Snackbar.make(findViewById(R.id.activity_main), "FAILURE", Snackbar.LENGTH_LONG).show();
-                }
-            }
-        });
+            super.onCancelled();
+        }
     }
 }
