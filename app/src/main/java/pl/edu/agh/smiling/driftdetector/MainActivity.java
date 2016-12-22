0025dc0a02a592dc72ce3336cf62993231bbca58
Joanna Roczniak <roczniak.joanna@gmail.com>
2016-12-22 09:13:22 +0100

    Enable java8, add smile-math library, which produce: "Lambda coming from jar file need their interfaces on the classpath to be compiled, unknown interfaces are java.util.function.Function"

--- app/src/main/java/pl/edu/agh/smiling/driftdetector/MainActivity.java
+++ app/src/main/java/pl/edu/agh/smiling/driftdetector/MainActivity.java
@@ -1,12 +1,15 @@
 package pl.edu.agh.smiling.driftdetector;
 
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.kaazing.net.sse.SseEventReader;
 import org.kaazing.net.sse.SseEventSource;
@@ -19,7 +22,7 @@ import java.net.URISyntaxException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -32,10 +35,15 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.button_start)
     Button buttonStart;
 
+    //TODO: set localhost
     private final String host = "http://192.168.43.203:5000/subscribe";
     private SseEventSource sseEventSource;
 
     private AsyncTask asyncTask;
+
+//    private DriftDetector driftDetector;
+
+//    int classIndex = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.text_from_server);
 
         //TODO: Enable streaming from various sources
+//        driftDetector = DriftDetector.getInstance(100, "knn");
 
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,11 +84,21 @@ public class MainActivity extends AppCompatActivity {
                 final SseEventReader sseEventReader = sseEventSource.getEventReader();
 
                 SseEventType type = sseEventReader.next();
+                int iterator = 0;
+
+//                DriftDetector driftDetector = DriftDetector.getInstance(3, "knn");
+
+//                driftDetector.isDriftDetected();
+
                 while (type != SseEventType.EOS) {
+
                     Log.d(TAG, "onClick: new event");
                     if (type != null && type.equals(SseEventType.DATA)) {
                         CharSequence data = sseEventReader.getData();
+
+                        //TODO: Parse data to Attribute in smiling.
                         //TODO: Send new item to DriftDetector method and show when concept drift occurs
+//                        driftDetector.update(data.toString(), classIndex, iterator++);
                         publishProgress(data.toString());
                         Log.d(TAG, "onClick: " + data.toString());
                     } else {
